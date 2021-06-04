43cdad69ab1ff045c8071a812f3cbd8db4c6c408
MarcelBragance <bragance.m@hotmail.fr>
2021-06-04 20:16:37 +0200

    payment fin

--- src/Controller/Stripe/StripeCancelPaymentController.php
+++ src/Controller/Stripe/StripeCancelPaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Stripe;
 
+use App\Entity\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class StripeCancelPaymentController extends AbstractController
 {
     /**
-     * @Route("/stripe-payment-cancel", name="stripe_payment_cancel")
+     * @Route("/stripe-payment-cancel/{StripCheckoutSessionId}", name="stripe_payment_cancel")
      */
-    public function index(): Response
+    public function index(?Order $order): Response
     {
+        if(!$order || $order->getUser() !== $this->getUser()){
+            return $this->redirectToRoute("home");
+        }
+
         return $this->render('stripe/stripe_cancel_payment/index.html.twig', [
             'controller_name' => 'StripeCancelPaymentController',
+            'order'=> $order
         ]);
     }
 }
